9b4f10e82f39dc46fbbd854e61618c054e9034d5
ivnamo <32225071+ivnamo@users.noreply.github.com>
2025-07-04 10:52:23 +0200

    Update editor.py

--- editor.py
+++ editor.py
@@ -1,3 +1,11 @@
+
+# ------------------------------------------------------------------------------
+# FORMULATOR – Uso exclusivo de Iván Navarro
+# Todos los derechos reservados © 2025
+# Este archivo forma parte de un software no libre y no está autorizado su uso
+# ni distribución sin consentimiento expreso y por escrito del autor.
+# ------------------------------------------------------------------------------
+
 import streamlit as st
 from families import obtener_familias_parametros
 
