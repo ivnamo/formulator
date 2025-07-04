655c2e8548d65e4d2ddf4cf25d9988b258371957
ivnamo <32225071+ivnamo@users.noreply.github.com>
2025-07-04 10:52:44 +0200

    Update families.py

--- families.py
+++ families.py
@@ -1,4 +1,10 @@
-# families.py
+# ------------------------------------------------------------------------------
+# FORMULATOR – Uso exclusivo de Iván Navarro
+# Todos los derechos reservados © 2025
+# Este archivo forma parte de un software no libre y no está autorizado su uso
+# ni distribución sin consentimiento expreso y por escrito del autor.
+# ------------------------------------------------------------------------------
+
 
 from collections import defaultdict
 
