792c5e7135537b5829742edd11c6687deb6fda4d
ivnamo <32225071+ivnamo@users.noreply.github.com>
2025-07-04 19:31:26 +0200

    Update orden_editor.py

--- utils/orden_editor.py
+++ utils/orden_editor.py
@@ -1,5 +1,9 @@
-# utils/orden_editor.py
-
+# ------------------------------------------------------------------------------
+# FORMULATOR – Uso exclusivo de Iván Navarro
+# Todos los derechos reservados © 2025
+# Este archivo forma parte de un software no libre y no está autorizado su uso
+# ni distribución sin consentimiento expreso y por escrito del autor.
+# ------------------------------------------------------------------------------
 import streamlit as st
 import pandas as pd
 
