f16ab610cfcc51ab59b9ed8df7482df0359d404f
ivnamo <32225071+ivnamo@users.noreply.github.com>
2025-07-14 12:25:34 +0200

    Update optimizador_simplex.py

--- utils/optimizador_simplex.py
+++ utils/optimizador_simplex.py
@@ -1,4 +1,9 @@
-# utils/optimizador_simplex.py
+# ------------------------------------------------------------------------------
+# FORMULATOR – Uso exclusivo de Iván Navarro
+# Todos los derechos reservados © 2025
+# Este archivo forma parte de un software no libre y no está autorizado su uso
+# ni distribución sin consentimiento expreso y por escrito del autor.
+# ------------------------------------------------------------------------------
 
 import numpy as np
 import pandas as pd
