67c1200ffebc5585962bf33026edc4e71909c45c
ivnamo <32225071+ivnamo@users.noreply.github.com>
2025-07-16 12:11:31 +0200

    Update vista_calidad.py

--- crud_calidad/vista_calidad.py
+++ crud_calidad/vista_calidad.py
@@ -1,4 +1,9 @@
-# crud_calidad/vista_calidad.py
+# ------------------------------------------------------------------------------
+# FORMULATOR – Uso exclusivo de Iván Navarro
+# Todos los derechos reservados © 2025
+# Este archivo forma parte de un software no libre y no está autorizado su uso
+# ni distribución sin consentimiento expreso y por escrito del autor.
+# ------------------------------------------------------------------------------
 
 import streamlit as st
 import pandas as pd
