c7cea1732595e6a55f3f40ee8a04f7de1c79084f
ivnamo <32225071+ivnamo@users.noreply.github.com>
2025-07-16 12:11:23 +0200

    Update update_calidad.py

--- crud_calidad/update_calidad.py
+++ crud_calidad/update_calidad.py
@@ -1,3 +1,10 @@
+# ------------------------------------------------------------------------------
+# FORMULATOR – Uso exclusivo de Iván Navarro
+# Todos los derechos reservados © 2025
+# Este archivo forma parte de un software no libre y no está autorizado su uso
+# ni distribución sin consentimiento expreso y por escrito del autor.
+# ------------------------------------------------------------------------------
+
 from utils.supabase_client import supabase
 
 def actualizar_estado_calidad(calidad_id: str, nuevo_estado: str, nuevas_observaciones: str = ""):
